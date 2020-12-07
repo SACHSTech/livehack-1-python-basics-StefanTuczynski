0b38bafcf5f1dd39dc89878fafbd5a844f408051
Stefan Valdemar Valdemar <stefanvaldemar.tuczynski23@ycdsbk12.ca>
2020-12-07 18:26:55 +0000

    Finished Version

--- problem1.py
+++ problem1.py
@@ -1,1 +1,18 @@
+"""
+Name:   problem1.py
+Purpose: This problem requires a celsius to fahrenheit converter to help the user 
+understand the amount of degrees celsius to fahrenheit
 
+
+Author: Tuczynski.S
+
+Date:   07/12/2020
+"""
+
+#inputs user data for the amount of degrees celsius
+print("--- Welcome to the online celsius to fahrenheit converter ---")
+c = float(input("Degrees Celsius: "))
+
+#outputs the amount of degrees fahrenheit after conversion
+print(c , "degrees celsius equals", float(c * (9/5) + 32), "degrees fahrenheit .")
+print("--- Thank you for using the online fahrenheit to celsius conveter! ---")
--- problem2.py
+++ problem2.py
@@ -1,1 +1,23 @@
+"""
+Name:   problem2.py
+Purpose: This problem requires the amount of chicken needed to distrubute to the 
+number of students + Mr. Fabroa (who gets the remaining pieces). This code will
+calculate the number of wings to fairly distribute to each classmate, but also
+Mr. Fabroa who recieves the rest.
+
+Author: Tuczynski.S
+
+Date:   07/12/2020
+"""
+
+#input data for amount of chicken pieces
+chicken = int(input("How many pieces of chicken are there: "))
+students = int(input("How many students are there: "))
+
+#computes data given by user and applies it to the problem
+distributed_pieces = round((chicken / students))
+fabroas_pieces = (chicken % students)
+
+#outputs the pieces each student gets and the pieces Mr.Fabroa gets
+print("Each student gets:", distributed_pieces, "and Mr.Fabroa gets:", fabroas_pieces)
 
